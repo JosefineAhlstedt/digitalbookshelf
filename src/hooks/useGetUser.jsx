e980fe514ade2e4b0ff1748854d8f9cb57d48fc2
Josefine Ahlstedt <josefine.ahlstedt@gmail.com>
2023-01-17 10:35:45 +0100

    Get a user with a specifik id

--- src/hooks/useGetUser.jsx
+++ src/hooks/useGetUser.jsx
@@ -2,14 +2,25 @@ import { db } from "../firebase";
 import { collection, query, where, getDoc, doc } from "firebase/firestore";
 import { useAuthContext } from "../contexts/authContext";
 
-const getUser = async () => {
-  const { currentUser } = useAuthContext();
-  if (currentUser().uid !== undefined) {
-    const docRef = doc(db, "users", currentUser().uid);
+const getUser = async (id) => {
+  if (!id) {
+    const { currentUser } = useAuthContext();
+    if (currentUser().uid !== undefined) {
+      const docRef = doc(db, "users", currentUser().uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        return docSnap.data();
+      } else {
+        // doc.data() will be undefined in this case
+        return console.log("No such document!");
+      }
+    }
+  } else {
+    const docRef = doc(db, "users", id);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
       return docSnap.data();
     } else {
       // doc.data() will be undefined in this case
